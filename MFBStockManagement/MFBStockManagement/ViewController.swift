808100cab56f364639c44cb7605765a57b4ffcb7
Gabyche <gaby2796@hotmail.Fr>
2017-10-19 10:49:15 +0200

    Updating basic ViewController

--- MFBStockManagement/MFBStockManagement/ViewController.swift
+++ MFBStockManagement/MFBStockManagement/ViewController.swift
@@ -13,13 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.updateDecoderUI), name: NSNotification.Name(rawValue: DECODER_DATA_RECEIVED_VC), object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateDecoderUI(_ notification: Notification) {
+        if notification.object != nil && (notification.object is String) {
+            scanresult.text = notification.object
+        }
+    }
 
 }
 

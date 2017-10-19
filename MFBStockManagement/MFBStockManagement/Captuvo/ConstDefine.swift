e29e37c12c86e87746633515d519ff6fc811737c
Gabyche <gaby2796@hotmail.Fr>
2017-10-19 10:29:18 +0200

    Adding CaptuvoSDK files (in Swift)

---
+++ MFBStockManagement/MFBStockManagement/Captuvo/ConstDefine.swift
@@ -0,0 +1,13 @@
+//
+//  ConstDefine.swift
+//  TestingCaptuvo
+//
+//  Created by Gabriel Morin on 17/10/2017.
+//  Copyright © 2017 Gabriel Morin. All rights reserved.
+//
+
+import Foundation
+
+let DECODER_DATA_RECEIVED_VC = "decoderReceivedDataFORViewController"
+let MSR_DATA_RECEIVED_VC = "MSRReceivedDataFORViewController"
+let PM_DATA_RECEIVED_VC = "PMReceivedDataFORViewController"

d727c0d871bb9674e307fa853a719597c7f02ccc
Gabyche <gaby2796@hotmail.Fr>
2017-10-31 10:21:28 +0100

    Adding Ressource Class

---
+++ MFB IOS Project/Samples/MFB_IOS_PROJECT/MFB_IOS_PROJECT/Stock.swift
@@ -0,0 +1,20 @@
+//
+//  Stock.swift
+//  MFB_IOS_PROJECT
+//
+//  Created by Gabriel Morin on 31/10/2017.
+//  Copyright © 2017 Honeywell Inc. All rights reserved.
+//
+
+import Foundation
+
+class Ressource {
+    var article: String!
+    var qtity: Int?
+    var colisage: Int?
+    var pckgPerLevel: Int?
+    var owner: String!
+    var date: Date!
+    var code : Captuvo!
+}
+

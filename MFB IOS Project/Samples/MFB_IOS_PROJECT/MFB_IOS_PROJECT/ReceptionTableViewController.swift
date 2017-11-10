a9f3162f4fd2c16ff1e19457b235b7579f757e15
Gabyche <gaby2796@hotmail.Fr>
2017-11-10 17:03:10 +0100

    adding searchbar code, bar does not appear

--- MFB IOS Project/Samples/MFB_IOS_PROJECT/MFB_IOS_PROJECT/ReceptionTableViewController.swift
+++ MFB IOS Project/Samples/MFB_IOS_PROJECT/MFB_IOS_PROJECT/ReceptionTableViewController.swift
@@ -13,16 +13,30 @@ import SwiftyJSON
 
 class ReceptionTableViewController: UITableViewController {
 
+    
+
 
     @IBOutlet var ProductTableView: UITableView!
     
-   var productName = [String]()
     
+    let searchController = UISearchController(searchResultsController: nil)
+    var productName = [String]()
+    var filteredProductName = [String]()
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search product name"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
         
         Alamofire.request(ProductRouter.get).responseJSON { response in
             // check for errors
@@ -53,13 +67,6 @@ class ReceptionTableViewController: UITableViewController {
 
             self.ProductTableView.reloadData()
         }
-        
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -74,58 +81,52 @@ class ReceptionTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(self.productName.count)
-        return self.productName.count
+        if isFiltering() {
+            return filteredProductName.count
+        }
+        
+        return productName.count
     }
+
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel!.text = productName[indexPath.row]
+        let name: String
+        if isFiltering() {
+            name = filteredProductName[indexPath.row]
+        } else {
+            name = productName[indexPath.row]
+        }
+        cell.textLabel!.text = name
         return cell
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // MARK: - Search Bar
+    
+
+    
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredProductName = productName.filter({( name : String) -> Bool in
+            return name.lowercased().contains(searchText.lowercased())
+        })
+        
+        self.ProductTableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
     }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+}
+
+
+extension ReceptionTableViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
